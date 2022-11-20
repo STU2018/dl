fd8a23d48f8ff33cce671c049abc5fd4ff41257d
ljh <ljh2024@foxmail.com>
2022-11-20 22:03:21 +0800

    create nd array

--- data calculate/create nd array.py
+++ data calculate/create nd array.py
@@ -7,14 +7,12 @@ print(x.shape)
 print(x.size)
 
 X = x.reshape((3, 4))
-# X=x.reshape((-1,4))
-# X=x.reshape((3,-1))
 print(X)
 
 print(nd.zeros((2, 3, 4)))
 print(nd.ones((3, 4)))
 
-Y = nd.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+Y = nd.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])#用list构建array
 print(Y)
 
 # 均值为0，标准差为1
