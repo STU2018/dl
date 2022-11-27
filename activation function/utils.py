80d1c9ac5582cd7cfd7f07077980b11751d87a5f
ljh <ljh2024@foxmail.com>
2022-11-27 12:17:11 +0800

    plot script

---
+++ activation function/utils.py
@@ -0,0 +1,8 @@
+import d2lzh as d2l
+def xyplot(x_vals, y_vals, name):
+    d2l.set_figsize(figsize=(5, 2.5))
+    d2l.plt.plot(x_vals.asnumpy(), y_vals.asnumpy())
+    d2l.plt.xlabel('x')
+    d2l.plt.ylabel(name + '(x)')
+    d2l.plt.show()
+
